12b29706aab86111a0b64ad3c11a01c443ef8056
slinkyfever <160118486+slinkyfever@users.noreply.github.com>
2024-06-03 16:32:37 +0800

    Add files via upload

--- products.php
+++ products.php
@@ -45,8 +45,7 @@ if(isset($_POST['add_to_cart'])){
    <title>Products</title>
 
    <!-- Font Awesome CDN link -->
-   <link href="https://db.onlinewebfonts.com/c/11eae19d5201ee5e6b1c2ae903ff4ea6?family=Metal+Vengeance" rel="stylesheet">
-   
+<link href="https://db.onlinewebfonts.com/c/11eae19d5201ee5e6b1c2ae903ff4ea6?family=Metal+Vengeance" rel="stylesheet">
    <!-- Custom CSS file link -->
    <link rel="stylesheet" href="css/nav_user.css">
 </head>
@@ -83,7 +82,7 @@ if(isset($message)){
          <div class="box">
             <img src="uploaded_img/<?php echo $fetch_product['image']; ?>" alt="">
             <h3><?php echo $fetch_product['name']; ?></h3>
-            <div class="price">$<?php echo $fetch_product['price']; ?>/-</div>
+            <div class="price">Php <?php echo $fetch_product['price']; ?>/-</div>
             <input type="hidden" name="product_name" value="<?php echo $fetch_product['name']; ?>">
             <input type="hidden" name="product_price" value="<?php echo $fetch_product['price']; ?>">
             <input type="hidden" name="product_image" value="<?php echo $fetch_product['image']; ?>">
